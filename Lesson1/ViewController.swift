cb60fcc120fc0258f01bc9e063557ff4864d92f0
Mark <khasyanova.all@yandex.ru>
2020-08-20 12:43:19 +0300

    Initial Commit

---
+++ Lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Марк Мирзоян on 20.08.2020.
+//  Copyright © 2020 Марк Мирзоян. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
